769259a3e0de75597cabb2fb015aa4da9e30657b
ABgitme <abebekezhabesha@gmail.com>
2024-02-28 00:12:12 +0300

    update header

--- binary_trees.h
+++ binary_trees.h
@@ -62,7 +62,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 /*function that finds the sibling of a node*/
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-
+/* function that finds the uncle of a node*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /*Binary Search Tree*/
 typedef struct binary_tree_s bst_t;
 /*AVL Tree*/
