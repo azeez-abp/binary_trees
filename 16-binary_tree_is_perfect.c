eda353be2a1e7072f1cc653d13bd9fb9cee9b51e
ABgitme <abebekezhabesha@gmail.com>
2024-02-28 00:07:21 +0300

    betty 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to check
@@ -9,7 +8,7 @@
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int left_height;
-    int right_height;
+	int right_height;
 
 	if (tree == NULL)
 		return (0);
